56fad23e72195897942e11b38da25d5428ab65ca
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2025-12-28 13:21:21 +0530

    test1

--- app/app/user/logout/page.tsx
+++ app/app/user/logout/page.tsx
@@ -6,7 +6,6 @@ function page() {
       <h1>LOGOUT</h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-        quam eos deserunt quibusdam consectetur quaerat earum sapiente iste
       </p>
     </div>
   )
