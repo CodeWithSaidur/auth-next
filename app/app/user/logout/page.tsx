9438a55a166f604b0974e83a0c91a8101fc1b640
CodeWithSaidur <sabedbarbhuiya3@gmail.com>
2025-12-28 13:01:42 +0530

    add

--- app/app/user/logout/page.tsx
+++ app/app/user/logout/page.tsx
@@ -1,7 +1,15 @@
-const page = () => {
+import React from 'react'
+
+function page() {
   return (
     <div>
-      <h1>logout Page</h1>
+      <h1>LOGOUT</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
+        quam eos deserunt quibusdam consectetur quaerat earum sapiente iste
+        aperiam nisi vitae, culpa animi odit quod exercitationem dolore ducimus
+        tempore mollitia.
+      </p>
     </div>
   )
 }
